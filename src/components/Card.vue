cd3f4e695dad01daff6deec9cbafe415dcdd8d49
Larissa Dantier <larissa_dantier@hotmail.com>
2025-02-25 09:31:19 -0300

    feat: remove optional in interface

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,14 +2,14 @@
 
   defineProps<{
     name: string,
-    src?: string,
-    redirect?: () => void,
+    src: string,
+    redirect: () => void,
   }>(); 
 </script>
 
 
 <template>
-  <div @click="redirect && redirect()" class="flex flex-col justify-center items-center h-full bg-white p-4 rounded-xl cursor-pointer gap-4.5">
+  <div @click="redirect()" class="flex flex-col justify-center items-center h-full bg-white p-4 rounded-xl cursor-pointer gap-4.5">
     <img :src="src" class="max-w-[219px] max-h-[131px] rounded-xl">
     <span class="text-xl font-bold text-neutral-700 text-center">{{ name }}</span>
   </div>
